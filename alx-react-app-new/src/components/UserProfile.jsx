dd50f235d84c13a184a583b585003a014e64d752
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-07-26 20:47:22 +0100

    task2

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,9 @@
- const UserProfile = (props) => {
-   return (
-     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-        <h2 style={{ color: 'blue' }}>{props.name}</h2>
-        <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-        <p>Bio: {props.bio}</p>
-      </div>
-   );
- };
- export default UserProfile
+// ProfilePage.jsx
+import React from 'react';
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
