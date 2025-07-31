ad2078aa2a79167d91145edbaf4a114eea2b4b60
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-07-31 17:25:37 +0100

    task 0

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import RecipeList from './components/RecipeList'
+import AddReciepeForm from './components/AddReciepeForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <RecipeList />
+      <AddReciepeForm />
+    </div>
+  )
+}
+
+export default App
---
+++ recipe-sharing-app/src/components/AddReciepeForm.jsx
@@ -0,0 +1,34 @@
+// AddReciepeForm component
+import { useState } from "react";
+import { useRecipeStore } from "./recipeStore";
+
+export default function AddReciepeForm () {
+    const addRecipe = useRecipeStore(state => state.addRecipe)
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        addRecipe({id: Date.now(), title, description})
+        setTitle('')
+        setDescription('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" 
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}
+            placeholder="Title"
+            />
+
+            <textarea 
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
+            />
+
+            <button type="submit">Add Recipe</button>
+        </form>
+    )
+}
